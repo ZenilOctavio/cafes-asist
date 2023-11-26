8c2259121b2145dc257ada09f381174ac9270f13
Octavio Zenil <octaviozenil@gmail.com>
2023-11-26 01:17:41 +0000

    Registro model created

---
+++ app/models/registro.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Table, Column
+from sqlalchemy.sql.sqltypes import Integer, String, Boolean, DateTime
+from sql.database import meta, engine
+from typing import Literal
+
+tipo_registro = Literal['Entrada', 'Salida']
+
+registro = Table('Registro', meta, 
+                    Column('id_registro', Integer, primary_key=True),
+                    Column('id_integrante', Integer, nullable=False),
+                    Column('tipo_registro', String(7), nullable=False),
+                    Column('fecha_hora', DateTime, nullable=False)
+              )
+
+meta.create_all(engine)
